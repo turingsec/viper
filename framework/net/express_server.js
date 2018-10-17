0ea91b231e4f6513a6c0bbcf836d8eec18fd6f4b
e <shaoxingye@turingsec.com>
2018-10-17 12:28:41 +0800

    add wx xml

--- framework/net/express_server.js
+++ framework/net/express_server.js
@@ -9,28 +9,31 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 require('body-parser-xml')(bodyParser);
 const RateLimit = require('express-rate-limit');
-const debug = require('debug')('myapp:server');
 const http = require('http');
 const https = require('https');
 const mlogger = require("../../common/mlogger");
 const favicon = require('serve-favicon');
+const vhost = require('vhost');
 
-var express_server = function (_, opts) {
+var express_server = function(_, opts){
     opts = opts || {};
 
-    this.server = null;
+    this.servers = [];
+    this.should_call_cb = false;
 
-    this.use_https = opts.use_https;
     this.port = opts.port;
     this.secret = opts.secret;
+    this.subdomain = opts.subdomain;
     this.routes = opts.routes;
-    this.cert = opts.cert;
+    this.ssl = opts.ssl;
     this.express = opts.express;
+    this.allow_origin = opts.allow_origin;
 };
 
-express_server.prototype.start = function (cb) {
+express_server.prototype.start = function(cb) {
     var app = express();
-    var port = this.port;
+    var count = this.port.length;
+    var self = this;
 
     /*
     app.use(new RateLimit({
@@ -41,7 +44,15 @@ express_server.prototype.start = function (cb) {
     */
     app.use(compression());
 
-    if (this.express) {
+    if(this.express){
+        if(!!this.express.extra){
+            app.use(this.express.extra);
+        }
+
+        if(!!this.express.https_require){
+            app.use(this.express.https_require);
+        }
+
         app.set('views', this.express.views);
         app.engine('html', require('ejs').renderFile);
         app.set('view engine', 'html');
@@ -51,10 +62,10 @@ express_server.prototype.start = function (cb) {
     }
 
     app.use(logger('dev'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.raw());
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(bodyParser.raw({limit: '50mb'}));
     app.use(bodyParser.text());
-    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.urlencoded({ extended: false }));
 // 解决微信支付通知回调数据
     app.use(bodyParser.xml({
         limit: '2MB', // Reject payload bigger than 1 MB
@@ -66,82 +77,117 @@ express_server.prototype.start = function (cb) {
     }));
     app.use(cookieParser(this.secret));
 
+    if(this.allow_origin){
+        app.all("*", function (req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+            res.header('Access-Control-Allow-Methods', 'POST, GET');
+
+            if (req.method == 'OPTIONS') {
+                res.send(200);
+            } else {
+                next();
+            }
+        });
+    }
+
+    //setting subdomain
+    for(let dst in this.subdomain){
+        app.use(vhost(dst, this.subdomain[dst]));
+    }
+
     //setting route
-    for (let dst in this.routes) {
+    for(let dst in this.routes){
         app.use(dst, this.routes[dst]);
     }
 
     // catch 404 and forward to error handler
-    app.use(function (req, res, next) {
+    app.use(function(req, res, next) {
         var err = new Error('Not Found');
         err.status = 404;
         next(err);
     });
 
     // error handler
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
         // set locals, only providing error in development
         res.locals.message = err.message;
         res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+        console.log(err);
         // render the error page
         res.status(err.status || 500);
-        console.log(err);
-        res.render('error');
+
+        if(req.vhost){
+            var hostname = req.vhost.host;
+            res.redirect(req.protocol + "://" + hostname.substring(hostname.indexOf(".") + 1) + "/error");
+        }else{
+            res.redirect(req.protocol + "://" + req.hostname + "/error");
+        }
     });
 
-    app.set('port', port);
+    this.port.forEach(function(item){
+        var server = null;
 
-    if (this.use_https) {
-        this.server = https.createServer({
-            key: fs.readFileSync(this.cert.key),
-            cert: fs.readFileSync(this.cert.cert)
-        }, app);
-    } else {
-        this.server = http.createServer(app);
-    }
+        app.set('port', item[0]);
 
-    this.server.listen(port, "0.0.0.0");
-
-    /**
-     * Event listener for HTTP server "error" event.
-     */
-    this.server.on('error', function (error) {
-
-        if (error.syscall !== 'listen') {
-            throw error;
+        if(item[1]){
+            server = https.createServer({
+                key: fs.readFileSync(self.ssl.key),
+                cert: fs.readFileSync(self.ssl.cert)
+            }, app);
+        }else{
+            server = http.createServer(app);
         }
 
-        var bind = typeof port === 'string'
-            ? 'Pipe ' + port
-            : 'Port ' + port;
+        server.listen(item[0], "0.0.0.0");
 
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-            case 'EACCES':
-                mlogger.error(bind + ' requires elevated privileges');
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                mlogger.error(bind + ' is already in use');
-                process.exit(1);
-                break;
-            default:
+        /**
+         * Event listener for HTTP server "error" event.
+         */
+        server.on('error', function(error){
+
+            if (error.syscall !== 'listen') {
                 throw error;
-        }
+            }
 
-    });
+            var bind = typeof item[0] === 'string'
+                ? 'Pipe ' + item[0]
+                : 'Port ' + item[0];
 
-    /**
-     * Event listener for HTTP server "listening" event.
-     */
-    this.server.on('listening', function () {
-        mlogger.info("express server listening on port " + port);
-        cb();
+            // handle specific listen errors with friendly messages
+            switch (error.code) {
+                case 'EACCES':
+                    mlogger.error(bind + ' requires elevated privileges');
+                    process.exit(1);
+                    break;
+                case 'EADDRINUSE':
+                    mlogger.error(bind + ' is already in use');
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+
+        });
+
+        /**
+         * Event listener for HTTP server "listening" event.
+         */
+        server.on('listening', function(){
+            mlogger.info("express server listening on port " + item[0]);
+            count -= 1;
+
+            if(count == 0){
+                cb();
+            }
+        });
+
+        self.servers.push(server);
     });
 };
 
-express_server.prototype.stop = function (cb) {
+express_server.prototype.stop = function(cb){
 
 };
 
