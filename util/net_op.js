4b34818e4e5053c0b2bb89614e1b93d57414b908
tyrande000 <tyrande000@gmail.com>
2019-02-16 20:27:05 +0800

    add net op

--- util/net_op.js
+++ util/net_op.js
@@ -2,19 +2,20 @@
  * Module dependencies.
  */
 const https = require('https');
+const http = require('http');
 
 var net_op = module.exports = {};
 
-net_op.download = function(options){
+net_op.request = function(obj, option, cb){
 	return new Promise((resolve, reject) => {
-		https.get(options, function(res) {
+		obj.get(option, function(res) {
 			var data = [], dataLen = 0;
 
 			res.on('data', function(chunk) {
 				data.push(chunk);
 				dataLen += chunk.length;
 			}).on('end', function() {
-				var buf = new Buffer(dataLen);
+				var buf = Buffer.alloc(dataLen);
 
 				for (var i=0, len = data.length, pos = 0; i < len; i++) {
 					data[i].copy(buf, pos);
@@ -27,4 +28,12 @@ net_op.download = function(options){
 			reject(e);
 		});
 	});
+}
+
+net_op.https_request = function(option){
+	return net_op.request(https, option);
+}
+
+net_op.http_request = function(option){
+	return net_op.request(http, option);
 }
