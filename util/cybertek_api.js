9fc592582464745b3191806b2a043edbb821c002
tyrande000 <tyrande000@gmail.com>
2019-03-24 19:04:24 +0800

    add json true

--- util/cybertek_api.js
+++ util/cybertek_api.js
@@ -12,6 +12,7 @@ cybertek_api.prototype.file_upload = async function(content, filename, config){
 	return await net_op.common_request({
 		url: `${config.URL}/file/upload`,
 		method: 'POST',
+		json: true,
 		formData: {
 			file: {
 				value: content,
@@ -27,20 +28,23 @@ cybertek_api.prototype.file_upload = async function(content, filename, config){
 cybertek_api.prototype.ipv4_geo = async function(ip, config){
 	return await net_op.common_request({
 		url: `${config.URL}/ipv4/geo?apikey=${config.APIKEY}&ip=${ip}`,
-		method: 'GET'
+		method: 'GET',
+		json: true
 	});
 }
 
 cybertek_api.prototype.ipv4_report = async function(ip, config){
 	return await net_op.common_request({
 		url: `${config.URL}/ipv4/report?apikey=${config.APIKEY}&ip=${ip}`,
-		method: 'GET'
+		method: 'GET',
+		json: true
 	});
 }
 
 cybertek_api.prototype.file_engine = async function(sha256, config){
 	return await net_op.common_request({
 		url: `${config.URL}/file/engine?apikey=${config.APIKEY}&file_hash=${sha256}`,
-		method: 'GET'
+		method: 'GET',
+		json: true
 	});
 }
