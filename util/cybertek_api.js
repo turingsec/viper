c1f5849e8f1917adb40888669f562d42d2ac34b1
tyrande000 <tyrande000@gmail.com>
2019-03-24 18:37:06 +0800

    bug fix

--- util/cybertek_api.js
+++ util/cybertek_api.js
@@ -8,33 +8,8 @@ function cybertek_api(){
 
 }
 
-cybertek_api.prototype.common_request = async function(options){
-	let ret = await net_op.common_request(options);
-	
-	if(ret.success){
-		switch(ret.body.code){
-			case macro.ERROR_CODE.SUCCESS:
-				return {
-					"success": true,
-					"body": ret.body
-				};
-			default:
-				return {
-					"success": false,
-					"msg": ret.body.msg,
-					"code": ret.body.code
-				};
-		}
-	}
-	
-	return {
-		"success": false,
-		"msg": ret.msg
-	};
-}
-
 cybertek_api.prototype.file_upload = function(content, filename, config){
-	return this.common_request({
+	return net_op.common_request({
 		url: `${config.URL}/file/upload`,
 		method: 'POST',
 		formData: {
@@ -50,21 +25,21 @@ cybertek_api.prototype.file_upload = function(content, filename, config){
 }
 
 cybertek_api.prototype.ipv4_geo = function(ip, config){
-	return this.common_request({
+	return net_op.common_request({
 		url: `${config.URL}/ipv4/geo?apikey=${config.APIKEY}&ip=${ip}`,
 		method: 'GET'
 	});
 }
 
 cybertek_api.prototype.ipv4_report = function(ip, config){
-	return this.common_request({
+	return net_op.common_request({
 		url: `${config.URL}/ipv4/report?apikey=${config.APIKEY}&ip=${ip}`,
 		method: 'GET'
 	});
 }
 
 cybertek_api.prototype.file_engine = function(sha256, config){
-	return this.common_request({
+	return net_op.common_request({
 		url: `${config.URL}/file/engine?apikey=${config.APIKEY}&file_hash=${sha256}`,
 		method: 'GET'
 	});
