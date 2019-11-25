7a86cbbb9910d4545470dae58846f4d8341be2fa
tyrande000 <tyrande000@gmail.com>
2019-11-25 18:31:41 +0800

    bug fix

--- util/file_detector.js
+++ util/file_detector.js
@@ -17,7 +17,7 @@ file_detector.prototype.tell = function(content){
 	return new Promise(function (resolve, reject) {
 		that.magic.detect(content, function(err, result) {
 			if (err){
-				resolve(null);
+				resolve("");
 			}else{
 				resolve(result);
 			}
