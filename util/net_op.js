8e51894a1e650b903a0c2b92124e0216605b26e7
tyrande000 <tyrande000@gmail.com>
2020-01-11 18:02:02 +0800

    add caller ip

--- util/net_op.js
+++ util/net_op.js
@@ -51,13 +51,24 @@ net_op.random_public_ipv4 = function () {
 	let self = this;
 	let ip = randomip('0.0.0.0', 0);
 
-	if(ip_mod.isPrivate(ip)){
+	if (ip_mod.isPrivate(ip)) {
 		return self.random_public_ipv4();
 	}
 
 	return ip;
 }
 
-net_op.is_private = function(ip){
+net_op.is_private = function (ip) {
 	return ip_mod.isPrivate(ip);
+}
+
+//获取用户 ip
+net_op.get_caller_ip = (request) => {
+	let ip = request.headers['x-forwarded-for'] ||
+		request.connection.remoteAddress ||
+		request.socket.remoteAddress ||
+		request.connection.socket.remoteAddress;
+	ip = ip.split(',')[0];
+	ip = ip.split(':').slice(-1); //in case the ip returned in a format: "::ffff:146.xxx.xxx.xxx"
+	return ip;
 }
